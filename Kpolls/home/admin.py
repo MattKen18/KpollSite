de68452864304d7389937545dde07d06e33d4df4
Matthew Carby <carbymatthew808@gmail.com>
2020-07-19 14:04:16 -0500

    complete project

--- Kpolls/home/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Recommendation, Idolranking
-
-# Register your models here.
-
-
-admin.site.register(Recommendation)
-admin.site.register(Idolranking)
